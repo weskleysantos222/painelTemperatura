4b08343d51135f959d3e32c5e368fda5712b699b
Weskley Santos <santosweskley@hotmail.com>
2022-04-13 13:45:17 -0300

    função para aumentar e diminuir o contador de temperatura

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+
+
 function AbrirConfiguracoes(){
   document.getElementById("configuracoes").style.visibility = "visible";
 }
 function FecharConfiguracoes(){
   document.getElementById("configuracoes").style.visibility = "hidden";
+}
+
+function LigarDesligarAr(){
+
+  let arTemperatura = document.getElementById("ar-temperatura");
+
+  
+    arTemperatura = arTemperatura.style.borderColor = "green";
+    lblTemperatura = lblTemperatura.style.color = "green";
+    
+
+}
+
+function AumentarTemperatura(){
+  let txtTemperatura = document.getElementById("txtTemperatura");
+
+  if(txtTemperatura.value >= 32){
+    alert("Temperatura não pode ser maior que 32°c")
+  }
+  else{
+    txtTemperatura.value++;
+  }
+
+}
+function DiminuirTemperatura(){
+  let txtTemperatura = document.getElementById("txtTemperatura");
+  if(txtTemperatura.value <= 16){
+    alert("Temperatura não pode ser menor que 16°c")
+  }
+  else{
+  txtTemperatura.value--;
+  }
+
 }
